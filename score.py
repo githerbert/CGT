2ffef44e0bace36457c63d155c414aa67416c918
Unknown <ceemon@gmx.net>
2018-01-19 16:20:09 +0100

    Deleted unnecessary data

--- score.py
+++
@@ -1,9 +0,0 @@
-class Score:
-
-    def __init__(self):
-
-        self.scorelist = []
-
-    def addScore(self, code_id, paper_id, sent_id ,score):
-        my_tuple = (code_id, paper_id, sent_id, score)
-        self.scorelist.append(my_tuple)
